559222941cc0762483dedca398c18b708fe66b09
EvertonBorges <evertonsoaresborges@gmail.com>
2020-05-24 23:09:46 -0400

    feat - service using threads

--- app/src/main/java/br/com/cesar/emailprocessorservice/EmailService.java
+++ app/src/main/java/br/com/cesar/emailprocessorservice/EmailService.java
@@ -2,47 +2,65 @@ package br.com.cesar.emailprocessorservice;
 
 import android.app.Service;
 import android.content.Intent;
-import android.os.Binder;
-import android.os.Handler;
-import android.os.HandlerThread;
 import android.os.IBinder;
-import android.os.Looper;
-import android.os.Message;
-import android.util.Log;
-import android.widget.Toast;
 
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 
 import br.com.cesar.emailprocessorservice.util.Utils;
 
 public class EmailService extends Service {
 
-    public EmailService() {
-    }
+    private final List<MyThread> threads = new ArrayList<>();
 
     @Override
-    public int onStartCommand(Intent intent, int flags, int startId) {
-        ArrayList<String> emails = intent.getStringArrayListExtra("EMAILS");
-        Utils.removeDuplicates(emails);
-        intent.putStringArrayListExtra("EMAILS", emails);
+    public int onStartCommand(final Intent intent, int flags, int startId) {
+        MyThread thread = new MyThread(intent, startId);
+        threads.add(thread);
+
+        thread.start();
 
         return START_NOT_STICKY;
     }
 
     @Override
     public void onDestroy() {
-
+        super.onDestroy();
+        for(MyThread thread: threads) {
+            thread.interrupt();
+        }
     }
 
     @Override
     public void onCreate() {
+        super.onCreate();
     }
 
     @Override
     public IBinder onBind(Intent intent) {
-        return new Binder();
+        return null;
+    }
+
+    class MyThread extends Thread {
+        private final Intent intent;
+        private final int startId;
+
+        MyThread(Intent intent, int startId) {
+            this.intent = intent;
+            this.startId = startId;
+        }
+
+        @Override
+        public void run() {
+            super.run();
+
+            try {
+                ArrayList<String> emails = intent.getStringArrayListExtra("EMAILS");
+                Utils.removeDuplicates(emails);
+                intent.putStringArrayListExtra("EMAILS", emails);
+            } catch (Exception ignored) { }
+            stopSelf(startId);
+        }
     }
 
 }
